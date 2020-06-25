c5bfcbdb79d32b70fc77d373b93ba85239de9d45
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-25 07:40:21 +0430

    Added shell script for approve

---
+++ shell-test-network/approve.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "==================== Start... ==================== "
+echo "Approveing in ${CORE_PEER_LOCALMSPID} peer..."
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name testchain --version 1.0 --package
+-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pe
+m
+
+echo "==================== Done! ==================== "
+echo ""
+echo ""
