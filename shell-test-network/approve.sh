515d91ee23e023b194033b55248b1fafdd5d7bc0
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-25 08:05:37 +0430

    Added commit and fixed approve bug

--- shell-test-network/approve.sh
+++ shell-test-network/approve.sh
@@ -3,11 +3,10 @@
 echo ""
 echo ""
 echo "==================== Start... ==================== "
-echo "Approveing in ${CORE_PEER_LOCALMSPID} peer..."
-peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name testchain --version 1.0 --package
--id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pe
-m
-
+echo "Approveing in $CORE_PEER_LOCALMSPID peer..."
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name testchain --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+echo ""
+echo ""
 echo "==================== Done! ==================== "
 echo ""
 echo ""
---
+++ shell-test-network/commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "==================== Start... ==================== "
+echo ""
+echo ""
+
+echo "Commiting the contract to peers"
+peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name testchain --version 1.0 --sequence 1 --tls --cafile ${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem --peerAddresses localhost:7051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses localhost:9051 --tlsRootCertFiles ${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+
+echo ""
+echo ""
+echo "==================== Done! ==================== "
+echo ""
+echo ""
